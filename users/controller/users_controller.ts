20e2bde1c2a2fc2eb846474cfe8d33a226110603
JayCode <vsquare396@gmail.com>
2023-08-03 15:49:36 +0100

    replaced mailgun with nodemailer

--- users/controller/users_controller.ts
+++ users/controller/users_controller.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import Mailgun from 'mailgun-js';
+import nodemailer from "nodemailer";
 
 class UsersController {
  
 async categories(req: express.Request, res: express.Response) {
     try {
-        
          res.status(200).send({
             status: true,
             message: "Successfully retrieved categories",
@@ -23,21 +22,30 @@ async sendMessage(req: express.Request, res: express.Response) {
         try {
             const {name, email, message} = req.body;
             
-          
-         await Mailgun({
-                apiKey: process.env.MAILGUN_DOMAIN as string,
-                domain: process.env.MAILGUN_API_KEY as string,
-             }).messages().send({
-                from: process.env.MAILGUN_SENDER as string,
-                to: email,
-                subject: `Welcome ${name}`,
-                text: message
-             });
-    
-             res.status(200).send({
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            secure: true,
+            port: 465,
+            auth: {
+              user: process.env.MAIL_SENDER as string,
+              pass: process.env.MAIL_PASSWORD as string,
+            }
+          });
+
+          const info = await transporter.sendMail({
+            from: process.env.MAIL_SENDER as string,
+            to: email,
+            subject: `Welcome, ${name}`,
+            text: message
+          });
+
+          if(info){
+            res.status(200).send({
                 status: true,
                 message: "Email sent successfully"
             });
+          }
+            
            } catch (error) {
             console.log(error);
             res.status(400).send({
